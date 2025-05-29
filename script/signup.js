31db0bf6c725aa4c728d703a4d0fdf93da80bb0b
Ric Charles Paquibot <ric2thiss@gmail.com>
2025-05-29 19:24:05 +0800

    Added more validations

--- script/signup.js
+++ script/signup.js
@@ -178,6 +178,165 @@ function emailValidation(){
 
 }
 
+function sexValidation(){
+      const form = document.forms["myForm"];
+      const sex = form["sex"].value;
+
+      if(!sex){
+        console.log("Sex is required! Please select for your identity.");
+        return false;
+      }
+
+      return true;
+}
+
+function addressValidation(){
+    const form = document.forms["myForm"];
+
+    const purok = form["purok"].value.trim();
+    const barangay = form["barangay"].value.trim();
+    const city = form["city"].value.trim();
+    const province = form["province"].value.trim();
+    const country = form["country"].value.trim();
+    const zip = form["zip"].value.trim();
+
+    if (!purok || !barangay || !city || !province || !country || !zip) {
+        console.log("All address fields are required.");
+        return false;
+    }
+
+    // --------------------------Checking if hasDoubleSpaces---------------------------------------- //
+
+    if(hasDoubleSpace(purok)){
+        console.log("Purok field should not have 2 or more white spaces.")
+        return false;
+    }
+
+    if(hasDoubleSpace(barangay)){
+        console.log("Barangay field should not have 2 or more white spaces.")
+        return false;
+    }
+
+    if(hasDoubleSpace(city)){
+        console.log("City field should not have 2 or more white spaces.")
+        return false;
+    }
+
+    if(hasDoubleSpace(province)){
+        console.log("Province field should not have 2 or more white spaces.")
+        return false;
+    }
+
+    if(hasDoubleSpace(country)){
+        console.log("Country field should not have 2 or more white spaces.")
+        return false;
+    }
+
+    if(hasDoubleSpace(zip)){
+        console.log("Zipcode field should not have 2 or more white spaces.")
+        return false;
+    }
+
+// -----------------------------------Checking the length------------------------------------------ //
+
+    if(!isMaxAndMinLengthValid(purok, 4, 20)){
+        console.log("Purok field must be at least 4 to 20 letters")
+        return false;
+    }
+
+    if(!isMaxAndMinLengthValid(barangay, 4, 20)){
+        console.log("Barangay field must be at least 4 to 20 letters")
+        return false;
+    }
+
+    if(!isMaxAndMinLengthValid(city, 4, 20)){
+        console.log("City field must be at least 4 to 20 letters")
+        return false;
+    }
+
+    if(!isMaxAndMinLengthValid(province, 4, 20)){
+        console.log("Province field must be at least 4 to 20 letters")
+        return false;
+    }
+
+    if(!isMaxAndMinLengthValid(country, 4, 20)){
+        console.log("Country field must be at least 4 to 20 letters")
+        return false;
+    }
+
+    if(!isMaxAndMinLengthValid(zip, 4, 6)){
+        console.log("Zipcode field must be at least 4 to 6 numbers")
+        return false;
+    }
+
+// ----------------------------Checking for word Formating ------------------------------------ //
+    if (!isWordFormatValid(purok)) {
+        console.log("Purok is invalid format! Please follow this format example 'Purok Bayabas'. Only in the first letter of every word must be capital letter.");
+        return false;
+    }
+
+    if (!isWordFormatValid(barangay)) {
+        console.log("Barangay is invalid format! Please follow this format example 'Bayanihan'. Only in the first letter of every word must be capital letter.");
+        return false;
+    }
+    
+    if (!isWordFormatValid(city)) {
+        console.log("City is invalid format! Please follow this format example 'Cabadbarna City'. Only in the first letter of every word must be capital letter.");
+        return false;
+    }
+
+
+    if (!isWordFormatValid(province)) {
+        console.log("Province is invalid format! Please follow this format example 'Agusan Del Norte'. Only in the first letter of every word must be capital letter.");
+        return false;
+    }
+    
+    if (!isWordFormatValid(country)) {
+        console.log("Country is invalid format! Please follow this format example 'Philippines'. Only in the first letter of every word must be capital letter.");
+        return false;
+    }
+
+    // Check the zipcode if has a letter then throw an error
+
+    if(hasLetter(zip)){
+        console.log("Zipcode should not have a letters. Please input a numbers only!");
+        return false;
+    }
+
+
+// -----------------------Checking if there are consecutive characters------------------------- //
+
+    if(hasConsecutiveChars(purok)){
+        console.log("Purok should not have 3 the same consecutive characters. Example: hhh or HHH");
+        return false;
+    }
+
+    if(hasConsecutiveChars(barangay)){
+        console.log("Barangay should not have 3 the same consecutive characters. Example: hhh or HHH");
+        return false;
+    }
+
+    if(hasConsecutiveChars(city)){
+        console.log("City should not have 3 the same consecutive characters. Example: hhh or HHH");
+        return false;
+    }
+
+    if(hasConsecutiveChars(province)){
+        console.log("Province should not have 3 the same consecutive characters. Example: hhh or HHH");
+        return false;
+    }
+
+    if(hasConsecutiveChars(country)){
+        console.log("Country should not have 3 the same consecutive characters. Example: hhh or HHH");
+        return false;
+    }
+
+
+    return true;
+
+
+}
+
 function validateRegForm(event) {
     event.preventDefault();
 
@@ -192,6 +351,12 @@ function validateRegForm(event) {
     if(!extentionNameValidation()) return false;
 
     if(!emailValidation()) return false;
+
+    if(!sexValidation()) return false;
+
+    if(!addressValidation()) return false;
+
+
     
 
     console.log("Form is valid");
